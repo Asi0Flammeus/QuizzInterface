9435978ccd65fe7a8ee1da3f6f38cef55e54fe64
Asi0Flammeus <asi0.flammeus@pm.me>
2023-10-28 23:10:25 +0200

    convert difficulty and reviewed in radio

--- readQuestion.py
+++ readQuestion.py
@@ -1,54 +1,56 @@
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, request
 import os
 import yaml
 
 app = Flask(__name__)
 
-# Declarar una variable global para el índice de la pregunta actual
+# Declare a global variable for the current question index
 current_question_index = 0
+
+def save_changes():
+    """Save changes to the current YAML file."""
+    global current_question_index, directorio_db, questions
+    actual_yaml = os.path.join(directorio_db, questions[current_question_index])
+    with open(actual_yaml, 'w') as w:
+        new_data = {
+            'course': request.form['course'],
+            'section': request.form['section'],
+            'chapter': request.form['chapter'],
+            'difficulty': request.form['difficulty'],
+            'duration': request.form['duration'],
+            'author': request.form['author'],
+            'tags': request.form.getlist('tag'),
+            'question': request.form['question'],
+            'answer': request.form['answer'],
+            'wrong_answers': request.form.getlist('wrong_answer'),
+            'explanation': request.form['explanation'],
+            'reviewed': request.form.get('reviewed', 'False')
+        }
+        yaml.dump(new_data, w)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global current_question_index, directorio_db, questions
+
     directorio_db = "./db/questions/"
     questions = os.listdir(directorio_db)
     questions.sort()
 
-    # Verificar si se ha presionado el botón 2 y actualizar el índice
     if request.method == 'POST':
-        if request.form.get('button1'):
-            print('Previous')
-            global current_question_index
+        if request.form.get('button1'):  # Previous button
+            save_changes()  # Save before moving
             current_question_index -= 1
-            # Asegurarse de no superar los límites de la lista
-            if current_question_index == 0:
-                current_question_index = 0
-        elif request.form.get('button2'):
-            print('Next')
-            current_question_index
+            if current_question_index < 0:
+                current_question_index = len(questions) - 1
+
+        elif request.form.get('button2'):  # Next button
+            save_changes()  # Save before moving
             current_question_index += 1
-            # Asegurarse de no superar los límites de la lista
             if current_question_index >= len(questions):
                 current_question_index = 0
-        elif request.form.get('button3'):
-            print('Save')
-            actual_yaml = os.path.join(directorio_db, questions[current_question_index])
-            with open(actual_yaml, 'w') as w:
-                new_data = {
-                    'course': request.form['course'],
-                    'section': request.form['section'],
-                    'chapter': request.form['chapter'],
-                    'difficulty': request.form['difficulty'],
-                    'duration': request.form['duration'],
-                    'author': request.form['author'],
-                    'tags': request.form.getlist('tag'),
-                    'question': request.form['question'],
-                    'answer': request.form['answer'],
-                    'wrong_answers': request.form.getlist('wrong_answer'),  # Obtener una lista de valores
-                    'explanation': request.form['explanation'],
-                    'reviewed': request.form['reviewed']
-                }
-                yaml.dump(new_data, w)
-               
+
+        elif request.form.get('button3'):  # Save button
+            save_changes()
 
     archivo_yaml = os.path.join(directorio_db, questions[current_question_index])
     with open(archivo_yaml, 'r') as f:
@@ -64,7 +66,6 @@ def index():
                            author=data['author'],
                            tags=data['tags'],
                            question=data['question'],
-                           questions=questions,
                            answer=data['answer'],
                            wrong_answers=data['wrong_answers'],
                            explanation=data['explanation'],
@@ -73,3 +74,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
